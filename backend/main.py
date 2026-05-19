51009df6d14984b5af449e3e71d6ceb5a48f32ef
Dewayne <r.dewayne.hannah@gmail.com>
2026-05-19 15:59:37 -0400

    Migrated main into backend repo.

---
+++ backend/main.py
@@ -0,0 +1,43 @@
+#Python 3.12.0
+#Version 0.0.0
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+import database
+from routers import journal, foods, goals
+
+
+# Database operations to create DB and verify tables
+database.create_database()
+database.create_database_table()
+database.create_journal_table()
+database.create_goals_table()
+
+app = FastAPI(
+    title="Nutrition Tracking API",
+    description="API to interact with program database",
+    version="0.0.0",
+    docs_url="/docs",
+)
+
+origin = "http://127.0.0.1:5173"
+
+
+# Adjust later for security
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Add API endpoint routing
+app.include_router(journal.router)
+app.include_router(foods.router)
+app.include_router(goals.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
