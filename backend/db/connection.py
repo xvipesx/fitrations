f2e59e0cc470acd883768e1f39d8a5b3c7578ae6
Dewayne <r.dewayne.hannah@gmail.com>
2026-07-18 12:16:18 -0400

    Comments to clarify requirement and usage.

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -1,4 +1,8 @@
 import os
 
+# Environment variables come directly from the Docker compose file the user can modify. We set the variables here
+# to ensure they are globally updated due to the import of this file across the other db files. 
+# Backup default option as ./backups included for development purposes.
+
 DB_PATH = os.environ.get("DB_PATH", "fitrations.db")
 BACKUP_DIR = os.environ.get("BACKUP_DIR", "./backups")
